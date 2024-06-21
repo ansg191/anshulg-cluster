f8e68dfe5c4f7bccddd818fac737ffb0e81ea0e0
Anshul Gupta <ansg191@anshulg.com>
2024-06-20 21:40:22 -0700

    Update `setup.sh` script

--- rpi5/setup.sh
+++ rpi5/setup.sh
@@ -11,15 +11,11 @@ set -eux
 # Set the hostname
 sudo hostnamectl set-hostname rpi5
 
+# Install microk8s
+sudo snap install microk8s --classic --channel=latest/edge
+
 # Install avahi
-# It doesn't work with pre-made ubuntu image, so we need to install it manually
-sudo snap install avahi
-
-# Touch avahi config file
-sudo touch /var/snap/avahi/common/etc/avahi/avahi-daemon.conf
-
-# Restart avahi service
-sudo snap restart avahi.daemon
+sudo apt-get install -y avahi-daemon
 
 # Start microk8s
 sudo microk8s start
@@ -34,19 +30,29 @@ sudo microk8s enable community
 sudo microk8s enable argocd
 sudo microk8s enable ingress:default-ssl-certificate=ingress/default-cert-tls
 
-# Setup mayastor
-sudo sysctl vm.nr_hugepages=1024
-echo 'vm.nr_hugepages=1024' | sudo tee -a /etc/sysctl.d/local.conf
-sudo modprobe nvme_tcp
-echo 'nvme-tcp' | sudo tee -a /etc/modules-load.d/microk8s-mayastor.conf
-
-sudo microk8s stop
-sudo microk8s start
-sudo microk8s status --wait-ready
-
-sudo microk8s enable core/mayastor --default-pool-size 20G
-#sudo microk8s mayastor-pools add --device /dev/nvme0n1
-
 # Export the kubeconfig
 mkdir -p ~/.kube
 sudo microk8s config | tee ~/.kube/config
+
+# Download kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+chmod +x kubectl
+
+# Verify the kubectl binary
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+rm kubectl.sha256
+
+# Install kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# Install open-iscsi and nfs-common
+sudo apt-get install -y open-iscsi nfs-common
+
+# Install iscsi module
+sudo modprobe iscsi_tcp
+echo 'iscsi_tcp' | sudo tee -a /etc/modules-load.d/iscsi.conf
+
+# Check longhorn environment compatibility
+curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.6.2/scripts/environment_check.sh | bash
