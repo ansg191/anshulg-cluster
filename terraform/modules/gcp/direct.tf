3a381d4a38992f12b9dd814816c203667b1c3486
Anshul Gupta <ansg191@anshulg.com>
2024-07-21 13:33:47 -0700

    Restrict dns01-solver IAM permissions

--- terraform/modules/gcp/direct.tf
+++ terraform/modules/gcp/direct.tf
@@ -11,12 +11,10 @@ resource "google_service_account" "dns01-solver" {
 }
 
 # Give the service account the ability to manage the DNS zone
-resource "google_project_iam_binding" "dns_admin" {
-	project = data.google_project.default.project_id
-	role    = "roles/dns.admin"
-	members = [
-		"serviceAccount:${google_service_account.dns01-solver.email}"
-	]
+resource "google_dns_managed_zone_iam_member" "direct-dns-admin" {
+	managed_zone = data.google_dns_managed_zone.direct.name
+	member       = "serviceAccount:${google_service_account.dns01-solver.email}"
+	role         = "roles/dns.admin"
 }
 
 # Add wildcard DNS record to 192.168.1.100
