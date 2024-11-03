873493e9c83d1a30452cb2a02d275da74ccf9654
Anshul Gupta <ansg191@anshulg.com>
2024-11-02 22:56:03 -0700

    Add restic backup bucket

--- terraform/modules/gcp/buckets.tf
+++ terraform/modules/gcp/buckets.tf
@@ -17,3 +17,25 @@ resource "google_storage_bucket_iam_member" "gke-test-bucket-default" {
 	member = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${data.google_project.default.project_id}.svc.id.goog/subject/ns/default/sa/default"
 	role   = "roles/storage.objectUser"
 }
+
+# Backup GKE Bucket
+resource "google_storage_bucket" "gke-backup-bucket" {
+	name          = "restic-backup-bucket-9r6c"
+	location      = "US"
+	force_destroy = true
+
+	uniform_bucket_level_access = true
+	storage_class               = "STANDARD"
+	public_access_prevention    = "enforced"
+
+	hierarchical_namespace {
+		enabled = true
+	}
+}
+
+# Bind GKE Bucket to restic service account in the restic  namespace
+resource "google_storage_bucket_iam_member" "gke-backup-bucket-restic" {
+	bucket = google_storage_bucket.gke-backup-bucket.name
+	member = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${data.google_project.default.project_id}.svc.id.goog/subject/ns/restic/sa/restic"
+	role   = "roles/storage.objectUser"
+}
