d21b5712e62f6368933716cfdec0c794bedee375
Anshul Gupta <ansg191@anshulg.com>
2024-11-02 17:40:36 -0700

    Give bucket permissions to GKE default account

--- terraform/modules/gcp/buckets.tf
+++ terraform/modules/gcp/buckets.tf
@@ -6,3 +6,9 @@ resource "google_storage_bucket" "gke-test-bucket" {
 	storage_class            = "STANDARD"
 	public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket_iam_member" "gke-test-bucket-default" {
+	bucket = google_storage_bucket.gke-test-bucket.name
+	member = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${data.google_project.default.project_id}.svc.id.goog/subject/ns/default/sa/default"
+	role   = "roles/storage.objectUser"
+}
