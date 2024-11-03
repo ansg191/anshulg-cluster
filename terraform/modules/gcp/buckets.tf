26b797f9063999aa90b86f6797350e986cf07300
Anshul Gupta <ansg191@anshulg.com>
2024-11-02 17:44:08 -0700

    Add comments to `buckets.tf`

--- terraform/modules/gcp/buckets.tf
+++ terraform/modules/gcp/buckets.tf
@@ -1,3 +1,6 @@
+# GKE Buckets
+
+# Test GKE Bucket
 resource "google_storage_bucket" "gke-test-bucket" {
 	name          = "gke-test-bucket-lvmn"
 	location      = "US"
@@ -7,6 +10,7 @@ resource "google_storage_bucket" "gke-test-bucket" {
 	public_access_prevention = "enforced"
 }
 
+# Bind GKE Bucket to default service account in the default namespace
 resource "google_storage_bucket_iam_member" "gke-test-bucket-default" {
 	bucket = google_storage_bucket.gke-test-bucket.name
 	member = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${data.google_project.default.project_id}.svc.id.goog/subject/ns/default/sa/default"
