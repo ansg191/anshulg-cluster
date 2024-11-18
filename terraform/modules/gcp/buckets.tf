5df1fcd2e1d674adb903c24c814391baa2a42575
Anshul Gupta <ansg191@anshulg.com>
2024-11-18 14:57:54 -0800

    Replace `miniflux` db with cockroachdb

--- terraform/modules/gcp/buckets.tf
+++ terraform/modules/gcp/buckets.tf
@@ -22,7 +22,7 @@ resource "google_storage_bucket_iam_member" "gke-test-bucket-default" {
 resource "google_storage_bucket" "gke-backup-bucket" {
 	name          = "restic-backup-bucket-9r6c"
 	location      = "US"
-	force_destroy = true
+	force_destroy = false
 
 	uniform_bucket_level_access = true
 	storage_class               = "STANDARD"
@@ -37,5 +37,23 @@ resource "google_storage_bucket" "gke-backup-bucket" {
 resource "google_storage_bucket_iam_member" "gke-backup-bucket-restic" {
 	bucket = google_storage_bucket.gke-backup-bucket.name
 	member = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${data.google_project.default.project_id}.svc.id.goog/subject/ns/restic/sa/restic"
+	role   = "roles/storage.objectUser"
+}
+
+# Cockroach Backup Bucket
+resource "google_storage_bucket" "gke-cockroach-bucket" {
+	name     = "cockroacchdb-backup-bucket-8kmcx"
+	location = "US"
+	force_destroy = false
+
+	uniform_bucket_level_access = true
+	storage_class = "COLDLINE"
+	public_access_prevention = "enforced"
+}
+
+# Bind GKE bucket to cockroachdb-sa service account in the miniflux namespace
+resource "google_storage_bucket_iam_member" "gke-cockroach-bucket-miniflux" {
+	bucket = google_storage_bucket.gke-cockroach-bucket.name
+	member = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${data.google_project.default.project_id}.svc.id.goog/subject/ns/miniflux/sa/cockroachdb-sa"
 	role   = "roles/storage.objectUser"
 }
