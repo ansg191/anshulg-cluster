ef036fc757bf1e6516b31ddf3e1cfb370ed6a18e
Anshul Gupta <ansg191@anshulg.com>
2024-12-15 15:08:59 -0800

    Add CUD pool to GKE

--- terraform/modules/gcp/compute.tf
+++ terraform/modules/gcp/compute.tf
@@ -1,18 +1,18 @@
 # region E2-Medium us-west1-a
 
-# resource "google_compute_reservation" "e2-medium" {
-#   name = "gke-reservation-frqbi"
-#   zone = "us-west1-a"
+resource "google_compute_reservation" "e2-medium" {
+  name = "gke-reservation-bdicp"
+  zone = "us-west1-a"
 
-#   specific_reservation {
-#     count = 2
-#     instance_properties {
-#       machine_type = "e2-medium"
-#     }
-#   }
+  specific_reservation {
+    count = 2
+    instance_properties {
+      machine_type = "e2-medium"
+    }
+  }
 
-#   specific_reservation_required = true
-# }
+  specific_reservation_required = true
+}
 
 # Create CUD for node
 # resource "google_compute_region_commitment" "e2-standard-2" {
@@ -35,62 +35,15 @@
 # }
 
 # Create node pool for the CUD reservation
-# resource "google_container_node_pool" "default_pool" {
-#   name       = "default-cud-pool"
-#   cluster    = google_container_cluster.default.id
-#   node_count = 2
-
-#   node_config {
-#     preemptible  = false
-#     machine_type = "e2-medium"
-
-#     service_account = google_service_account.default.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform",
-#     ]
-
-#     workload_metadata_config {
-#       mode = "GKE_METADATA"
-#     }
-
-#     reservation_affinity {
-#       consume_reservation_type = "SPECIFIC_RESERVATION"
-#       key                      = "compute.googleapis.com/reservation-name"
-#       values                   = [google_compute_reservation.e2-medium.name]
-#     }
-#   }
-
-#   management {
-#     auto_upgrade = true
-#   }
-# }
-
-# endregion E2-Medium us-west1-a
-# region E2-standard-2 us-west1-b
-
-resource "google_compute_reservation" "e2-standard-b" {
-  name = "gke-reservation-rvctu"
-  zone = "us-west1-b"
-
-  specific_reservation {
-    count = 1
-    instance_properties {
-      machine_type = "e2-standard-2"
-    }
-  }
-
-  specific_reservation_required = true
-}
-
-resource "google_container_node_pool" "default_pool_b" {
-  name       = "default-cud-pool-b"
+resource "google_container_node_pool" "default_pool" {
+  name       = "default-cud-pool"
   cluster    = google_container_cluster.default.id
   node_count = 2
-  location   = "us-west1-b"
+  location   = "us-west1-a"
 
   node_config {
     preemptible  = false
-    machine_type = "e2-standard-2"
+    machine_type = "e2-medium"
 
     service_account = google_service_account.default.email
     oauth_scopes = [
@@ -104,17 +57,13 @@ resource "google_container_node_pool" "default_pool_b" {
     reservation_affinity {
       consume_reservation_type = "SPECIFIC_RESERVATION"
       key                      = "compute.googleapis.com/reservation-name"
-      values                   = [google_compute_reservation.e2-standard-b.name]
+      values                   = [google_compute_reservation.e2-medium.name]
     }
   }
 
   management {
     auto_upgrade = true
   }
-
-  depends_on = [
-    google_compute_reservation.e2-standard-b
-  ]
 }
 
-# endregion E2-standard-2 us-west1-b
+# endregion E2-Medium us-west1-a
